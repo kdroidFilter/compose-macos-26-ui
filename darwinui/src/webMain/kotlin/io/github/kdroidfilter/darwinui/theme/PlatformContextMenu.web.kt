bd10b472e545412c5caa04dd945d4314a28dc0f7
Elie Gambache <elyahou.hadass@gmail.com>
2026-02-04 17:43:50 +0200

    Implement PlatformContextMenu for web: replace stub with actual rendering of the provided composable.

--- darwinui/src/webMain/kotlin/io/github/kdroidfilter/darwinui/theme/PlatformContextMenu.web.kt
+++ darwinui/src/webMain/kotlin/io/github/kdroidfilter/darwinui/theme/PlatformContextMenu.web.kt
@@ -3,6 +3,4 @@ package io.github.kdroidfilter.darwinui.theme
 import androidx.compose.runtime.Composable
 
 @Composable
-internal actual fun PlatformContextMenuOverride(content: @Composable (() -> Unit)) {
-    //TODO
-}
+internal actual fun PlatformContextMenuOverride(content: @Composable (() -> Unit)) = content()
